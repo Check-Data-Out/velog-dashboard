b7352cca8310c821fd35adbe6dae18e7d645ee42
Nuung <qlgks1@naver.com>
2023-11-20 01:01:29 +0900

    feature: sentry 추가 및 custom gtag 추가

--- nginx/pages/global.js
+++ nginx/pages/global.js
@@ -6,6 +6,16 @@
 
 // const DEFAULT_URL = "http://localhost:3000/api" // localonly
 const DEFAULT_URL = "https://velog-dashboard.kro.kr/api"
+Sentry.onLoad(function () {
+    Sentry.init({
+        // Performance Monitoring
+        tracesSampleRate: 1.0, // Capture 100% of the transactions
+        // Session Replay
+        replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+        replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    });
+});
+
 
 // ====================================================== //
 // Fetch & Api call wrapper
