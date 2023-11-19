d09fd9513832a9dd507d67892a532e0a26cee2d0
Nuung <qlgks1@naver.com>
2023-11-19 18:48:05 +0900

    feature: sample login 기능 추가

--- nginx/pages/index/index.js
+++ nginx/pages/index/index.js
@@ -10,15 +10,31 @@ const login = async () => {
     try {
         const res = await postData("/user", { accessToken, refreshToken });
         localStorage.setItem("userInfo", JSON.stringify(res.user));
-
-        // console.log(res);
         Swal.close();
         window.location.href = "/dashboard";
-        // window.location.href = "../dashboard/index.html";
     } catch (error) {
         await Swal.fire({
             title: "로그인 실패",
             text: `${error.message}, 만료된 토큰 이슈가 계속 발생하면 오른쪽 하단을 통해 바로 문의 주세요 도와드릴게요!!`,
+            icon: "error",
+            confirmButtonText: "OK",
+            background: "#242424", // 혹은 다크 테마에 맞는 색상으로 설정
+            color: "#fff", // 텍스트 색상을 흰색으로 설정
+        });
+    }
+};
+
+
+const sampleLogin = async () => {
+    try {
+        const res = await postData("/user/sample");
+        localStorage.setItem("userInfo", JSON.stringify(res.user));
+        Swal.close();
+        window.location.href = "/dashboard";
+    } catch (error) {
+        await Swal.fire({
+            title: "sample 로그인 실패",
+            text: error.message,
             icon: "error",
             confirmButtonText: "OK",
             background: "#242424", // 혹은 다크 테마에 맞는 색상으로 설정
@@ -38,6 +54,12 @@ const init = () => {
         event.preventDefault();
         showSpinner();
         await login();
+    });
+
+    document.getElementById("sample-login-btn").addEventListener("click", async (event) => {
+        event.preventDefault();
+        showSpinner();
+        await sampleLogin();
     });
 };
 
